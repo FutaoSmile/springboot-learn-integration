74cc63f04628b74db468880deaf15bc47fe74cc2
futao <1185172056@qq.com>
2019-09-19 10:46:39 +0800

    ServerSocket与Socket的请求与响应

--- learn-socket/src/main/java/com/futao/socket/Client.java
+++ learn-socket/src/main/java/com/futao/socket/Client.java
@@ -1,7 +1,8 @@
 package com.futao.socket;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
@@ -9,17 +10,36 @@ import java.nio.charset.StandardCharsets;
  * @author futao
  * Created on 2019/9/18.
  */
+@Slf4j
 public class Client {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             try {
                 Socket socket = new Socket("127.0.0.1", 8888);
                 OutputStream outputStream = socket.getOutputStream();
-                outputStream.write(String.format("我是【%s】号admin客户，我现在要登陆了", i).getBytes(StandardCharsets.UTF_8));
+                String format = String.format("我是【%s】号admin客户，我现在要登陆了", i);
+                log.info("客户端发送请求{}", format);
+                outputStream.write(format.getBytes(StandardCharsets.UTF_8));
                 outputStream.flush();
                 socket.shutdownOutput();
 
+                //接收服务器的响应
+                InputStream inputStream = socket.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String s;
+                while ((s = bufferedReader.readLine()) != null) {
+                    log.info("接收到响应{}", s);
+                }
+                socket.shutdownInput();
+
+                //资源都放在最后关闭
                 outputStream.close();
+                bufferedReader.close();
+                reader.close();
+                inputStream.close();
+
+                //断开socket
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
--- learn-socket/src/main/java/com/futao/socket/Server.java
+++ learn-socket/src/main/java/com/futao/socket/Server.java
@@ -1,9 +1,8 @@
 package com.futao.socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -16,13 +15,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author futao
  * Created on 2019/9/18.
  */
+@Slf4j
 public class Server {
     public static void main(String[] args) {
         AtomicInteger atomicInteger = new AtomicInteger(0);
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10, 10, TimeUnit.DAYS, new LinkedBlockingDeque<>(1024), r -> new Thread(r, "thread-socket-" + atomicInteger.getAndAdd(1)));
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 10, TimeUnit.DAYS, new LinkedBlockingDeque<>(1024), r -> new Thread(r, "thread-socket-" + atomicInteger.getAndAdd(1)));
         try {
             ServerSocket serverSocket = new ServerSocket(8888);
-            System.out.println("客户端等待连接.....");
+            log.info("客户端等待连接.....");
             while (true) {
                 Socket socket = serverSocket.accept();
                 executor.execute(() -> {
@@ -32,13 +32,25 @@ public class Server {
                         BufferedReader reader = new BufferedReader(inputStreamReader);
                         String s;
                         while ((s = reader.readLine()) != null) {
-                            System.out.println(s);
+                            log.info("服务器收到请求{}", s);
                         }
                         socket.shutdownInput();
+
+                        //响应
+                        OutputStream outputStream = socket.getOutputStream();
+                        outputStream.write("欢迎使用本系统".getBytes(StandardCharsets.UTF_8));
+                        outputStream.flush();
+                        socket.shutdownOutput();
+
+                        //资源都放在最后关闭
                         reader.close();
                         inputStreamReader.close();
                         inputStream.close();
+                        outputStream.close();
+
+                        //断开socket
                         socket.close();
+                        Thread.sleep(3000L);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
